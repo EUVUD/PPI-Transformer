4c36648dcc32528f5c57948f35a5abc47ade0673
EUVUD <Junning.David.Hu@gmail.com>
2025-10-18 18:07:20 -0400

    gitignore file added and download dataset function moved to dataset folder

--- data/download_huri.py
+++
@@ -1,7 +0,0 @@
-from tdc.multi_pred import PPI
-import os
-
-def download_huri():
-
-if __name__ == "__main__":
-    download_huri()
---
+++ dataset/download_huri.py
@@ -0,0 +1,19 @@
+from tdc.multi_pred import PPI
+
+def download_huri():
+    data = PPI(name = 'HuRI')
+    split = data.get_split()
+
+    # for key in split:
+    #     print(key)
+
+    train_data = split['train']
+    val_data = split['valid']
+    test_data = split['test']
+
+    train_data.to_csv('../data/huri_train.csv', index=False)
+    val_data.to_csv('../data/huri_val.csv', index=False)
+    test_data.to_csv('../data/huri_test.csv', index=False)
+
+if __name__ == "__main__":
+    download_huri()
