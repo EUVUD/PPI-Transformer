de7137b1a987f5fbbac28651becce6f3cae38b8a
EUVUD <Junning.David.Hu@gmail.com>
2025-10-27 17:36:56 -0400

    data module completed

--- dataset/ppiDataModule.py
+++ dataset/ppiDataModule.py
@@ -1,6 +1,6 @@
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader, random_split
-from torchvision import transforms, datasets
+from torch.utils.data import DataLoader
+from ppiDataset import ppiDataset
 
 class ppiDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str, batch_size: int = 32):
@@ -10,12 +10,9 @@ class ppiDataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         # Load your dataset here
-        if stage == 'train':
-            self.dataset = ppiDataset(f'{self.data_dir}/huri_train.csv')
-        elif stage == 'val':
-            self.dataset = ppiDataset(f'{self.data_dir}/huri_val.csv')
-        elif stage == 'test':
-            self.dataset = ppiDataset(f'{self.data_dir}/huri_test.csv')
+        self.train_dataset = ppiDataset(f'{self.data_dir}/huri_train.csv')
+        self.val_dataset = ppiDataset(f'{self.data_dir}/huri_val.csv')
+        self.test_dataset = ppiDataset(f'{self.data_dir}/huri_test.csv')
 
     def train_dataloader(self):
         return DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
